4340bbe0656fbda384d5f6c81467463a4088c5aa
杜琪 <duqi_2009@163.com>
2013-09-24 20:51:28 +0800

    简化了Import的java.io包结构

--- src/myJavaIODecorator/InputTest.java
+++ src/myJavaIODecorator/InputTest.java
@@ -1,10 +1,6 @@
 package myJavaIODecorator;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 public class InputTest {
 
