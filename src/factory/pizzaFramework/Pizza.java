3d1e640cfb4194f2d29803802d4eb26cc95e9a23
杜琪 <duqi_2009@163.com>
2013-09-26 10:21:37 +0800

    重写了Pizza类
    （1）Pizza类定义为abstract类，表示不能用new实例化Pizza；其他具体的类由Pizza派生；
    （2）在PizzaStore的orderPizza方法中，会定义一个Pizza变量，其实实例化的是具体的Pizza子类

--- src/factory/pizzaFramework/Pizza.java
+++ src/factory/pizzaFramework/Pizza.java
@@ -1,8 +1,37 @@
 package factory.pizzaFramework;
 
+import java.util.ArrayList;
+
 public abstract class Pizza {
-	public abstract void prepare();
-	public abstract void bake();
-	public abstract void cut();
-	public abstract void box();
+	protected String name;	//披萨名字
+	protected String dough;	//面团
+	protected String sauce;	//酱油
+	//披萨顶部的装饰食品
+	protected ArrayList<String> toppings = new ArrayList<String>();
+	
+	public void prepare() {
+		System.out.println("Preparing " + name);
+		System.out.println("Tossing dough...");
+		System.out.println("Adding sauce...");
+		System.out.println("Adding toppings...");
+		for (int i = 0; i < toppings.size(); i++) {
+			System.out.println("   " + toppings.get(i));
+		}
+	}
+	
+	public void bake(){
+		System.out.println("Bake for 25minutes at 350");
+	}
+	
+	public void cut(){
+		System.out.println("Cutting the pizza into diagonal slices");
+	}
+	
+	public void box(){
+		System.out.println("Place the pizza in the offical PizzaStore box");
+	}
+	
+	public String getName(){
+		return name;
+	}
 }
