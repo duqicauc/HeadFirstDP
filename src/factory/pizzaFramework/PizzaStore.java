761c88b3980041a0095581b75e3f48fbf18fe698
杜琪 <duqi_2009@163.com>
2013-09-26 08:46:59 +0800

    orderPizza函数定义为final
    在基类中，将某个函数定义为final，可以防止子类重写它

--- src/factory/pizzaFramework/PizzaStore.java
+++ src/factory/pizzaFramework/PizzaStore.java
@@ -7,7 +7,8 @@ public abstract class PizzaStore {
 	/**
 	 * 根据客户要求的类型定制不同种类的披萨
 	 * */
-	public Pizza orderPizza(String type) {
+	// final关键字确保该函数不被子类覆盖
+	public final Pizza orderPizza(String type) {
 		// 创建函数createPizza现在不是通过一个工厂对象来调用,而改为调用本类中的一个函数
 		Pizza pizza = createPizza(type);
 		// 披萨出货前的几项工作
