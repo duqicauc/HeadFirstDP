f758c18c013ef79c6ca5c637a7823308d41dadb2
杜琪 <duqi_2009@163.com>
2013-10-09 14:49:09 +0800

    完善ChocolateBoiler类
    （1）设定两个标识值表示“空”和“煮沸”的状态
    （2）三个动作：fill、drain、boil在进行之前，要检查现在的状态；在执行本操作后要修改相应状态；
    （3）问题：如果有两个ChocolateBoiler实例，则会出现问题。
    想到一个笑话：就是弟弟让家里妈妈和姐姐剪裤子，妈妈剪完之后没有留记号，姐姐也剪了，最后导致笑话；或者妈妈和姐姐都以为对方剪了，最后导致都没剪

--- src/singleton/ChocolateBoiler.java
+++ src/singleton/ChocolateBoiler.java
@@ -18,8 +18,19 @@ public class ChocolateBoiler {
 	}
 	
 	public void drain(){
-		
+		if (!isEmpty()&&isBoiled()) {
+			//drain the boiled milk and chocolate
+			empty = true;
+		}
 	}
+	
+	public void boil(){
+		if (!isEmpty()&&!isBoiled()) {
+			//bring the contents to a boil
+			boiled = true;
+		}
+	}
+	
 	public boolean isEmpty(){
 		return empty;
 	}
