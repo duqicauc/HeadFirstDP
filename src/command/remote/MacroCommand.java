2b9d433c93f391eecfc6737a53f37036d585de35
杜琪 <duqi_2009@163.com>
2013-10-12 15:39:06 +0800

    添加MacroCommand类，实现同时启动或关闭多个设备
    利用这个，再做一些扩展，可以让用户自定义一键模式，处理几个不同的家用设备

---
+++ src/command/remote/MacroCommand.java
@@ -0,0 +1,26 @@
+package command.remote;
+
+public class MacroCommand implements Command {
+	private Command[] commands;
+	
+	public MacroCommand(Command[] commands) {
+		this.commands = commands;
+	}
+	
+	@Override
+	public void execute() {
+		for (int i = 0; i < commands.length; i++) {
+			commands[i].execute();
+		}
+
+	}
+
+	@Override
+	public void undo() {
+		for (int i = 0; i < commands.length; i++) {
+			commands[i].execute();
+		}
+
+	}
+
+}
--- src/command/remote/RemoteLoader.java
+++ src/command/remote/RemoteLoader.java
@@ -1,5 +1,7 @@
 package command.remote;
 
+import factory.ingredient.MarinaraSauce;
+
 public class RemoteLoader {
 
 	/**
@@ -7,69 +9,102 @@ public class RemoteLoader {
 	 */
 	public static void main(String[] args) {
 		RemoteControl remote = new RemoteControl();
+//		
+//		Light livingRoomLight = new Light("Living Room");
+//		Light kitchenRoomLight = new Light("Kitchen Room");
+//		GarageDoor garageDoor = new GarageDoor("");
+//		Stereo stereo = new Stereo("Living Room");
+//		
+//		LightOnCommand livingRoomLightOn = new LightOnCommand(livingRoomLight);
+//		LightOffCommand livingRoomLightOff = new LightOffCommand(livingRoomLight);
+//		LightOnCommand kitchenRoomLightOn = new LightOnCommand(kitchenRoomLight);
+//		LightOffCommand kitchenRoomLightOff = new LightOffCommand(kitchenRoomLight);
+//		GarageDoorDownCommand garageDoorDown = new GarageDoorDownCommand(garageDoor);
+//		GarageDoorUpCommand garageDoorUp = new GarageDoorUpCommand(garageDoor);
+//		StereoOnWithCDCommand stereoOnWithCD = new StereoOnWithCDCommand(stereo);
+//		StereoOffCommand stereoOff = new StereoOffCommand(stereo);
+//		
+//		remote.setCommand(0,livingRoomLightOn, livingRoomLightOff);
+//		remote.setCommand(1, kitchenRoomLightOn, kitchenRoomLightOff);
+//		remote.setCommand(2, garageDoorUp, garageDoorDown);
+//		remote.setCommand(3, stereoOnWithCD, stereoOff);
+//		// 利用toString函数将插槽和在这个插槽上对应的命令对象打印出来
+//		System.out.println(remote);
+//		remote.undoButtonWasPushed();
+//		
+//		remote.onButtonWasPushed(0);
+//		remote.offButtonWasPushed(0);
+//		System.out.println(remote);
+//		remote.undoButtonWasPushed();
+//		//remote.onButtonWasPushed(1);
+//		//remote.offButtonWasPushed(1);
+//		//remote.onButtonWasPushed(2);
+//		//remote.offButtonWasPushed(2);
+//		//remote.undoButtonWasPushed();
+//		//remote.onButtonWasPushed(3);
+//		//remote.offButtonWasPushed(3);
+//		remote.offButtonWasPushed(0);
+//		remote.onButtonWasPushed(0);
+//		System.out.println(remote);
+//		remote.undoButtonWasPushed();
+//		
+//		
+//		/*测试CeilingFan相关undo功能*/
+//		CeilingFan livingRoomCeilingFan = new CeilingFan("Living Room");
+//		
+//		CeilingFanOnCommand ceilingFanOnCommand = new CeilingFanOnCommand(livingRoomCeilingFan);
+//		CeilingFanHighCommand ceilingFanHighCommand = new CeilingFanHighCommand(livingRoomCeilingFan);
+//		CeilingFanMediumCommand ceilingFanMediumCommand = new CeilingFanMediumCommand(livingRoomCeilingFan);
+//		CeilingFanLowCommand ceilingFanLowCommand = new CeilingFanLowCommand(livingRoomCeilingFan);
+//		CeilingFanOffCommand ceilingFanOffCommand = new CeilingFanOffCommand(livingRoomCeilingFan);
+//		
+//		remote.setCommand(0, ceilingFanOnCommand, ceilingFanOffCommand);
+//		remote.setCommand(1, ceilingFanLowCommand, ceilingFanOffCommand);
+//		remote.setCommand(2, ceilingFanMediumCommand, ceilingFanOffCommand);
+//		remote.setCommand(3, ceilingFanHighCommand, ceilingFanOffCommand);
+//		
+//		remote.onButtonWasPushed(0);
+//		remote.offButtonWasPushed(0);
+//		System.out.println(remote);
+//		remote.undoButtonWasPushed();//执行undo之后应该返回为low speed
+//		
+//		remote.onButtonWasPushed(3);
+//		remote.onButtonWasPushed(2);
+//		System.out.println(remote);
+//		remote.undoButtonWasPushed();//执行之后应该返回到remote.onButtonWasPushed(3)这句，即high speed
 		
-		Light livingRoomLight = new Light("Living Room");
-		Light kitchenRoomLight = new Light("Kitchen Room");
-		GarageDoor garageDoor = new GarageDoor("");
+		// 实例化设备对象
+		Light light = new Light("Living Room");
+		TV tv = new TV("Living Room");
 		Stereo stereo = new Stereo("Living Room");
-		
-		LightOnCommand livingRoomLightOn = new LightOnCommand(livingRoomLight);
-		LightOffCommand livingRoomLightOff = new LightOffCommand(livingRoomLight);
-		LightOnCommand kitchenRoomLightOn = new LightOnCommand(kitchenRoomLight);
-		LightOffCommand kitchenRoomLightOff = new LightOffCommand(kitchenRoomLight);
-		GarageDoorDownCommand garageDoorDown = new GarageDoorDownCommand(garageDoor);
-		GarageDoorUpCommand garageDoorUp = new GarageDoorUpCommand(garageDoor);
+		Hottub hottub = new Hottub();
+
+		// 实例化On开关命令对象
+		LightOnCommand lightOn = new LightOnCommand(light);
+		TVOnCommand tvOn = new TVOnCommand(tv);
 		StereoOnWithCDCommand stereoOnWithCD = new StereoOnWithCDCommand(stereo);
+		HottubOnCommand hottubOn = new HottubOnCommand(hottub);
+		
+		// 实例化Off开关命令对象
+		LightOffCommand lightOff = new LightOffCommand(light);
+		TVOffCommand tvOff = new TVOffCommand(tv);
 		StereoOffCommand stereoOff = new StereoOffCommand(stereo);
+		HottubOffCommand hotOff = new HottubOffCommand(hottub);
 		
-		remote.setCommand(0,livingRoomLightOn, livingRoomLightOff);
-		remote.setCommand(1, kitchenRoomLightOn, kitchenRoomLightOff);
-		remote.setCommand(2, garageDoorUp, garageDoorDown);
-		remote.setCommand(3, stereoOnWithCD, stereoOff);
-		// 利用toString函数将插槽和在这个插槽上对应的命令对象打印出来
+		// 将命令分类组装并构建两个分组命令对象
+		Command[] partyOn = {lightOn,tvOn,stereoOnWithCD,hottubOn};
+		Command[] partyOff = {lightOff,tvOff,stereoOff,hotOff};	
+		MacroCommand partyOnMacro = new MacroCommand(partyOn);
+		MacroCommand partyOffMarco = new MacroCommand(partyOff);
+		
+		// 将命令对象与插槽绑定
+		remote.setCommand(0, partyOnMacro, partyOffMarco);
+		
 		System.out.println(remote);
-		remote.undoButtonWasPushed();
-		
+		System.out.println("-----Push the partyOn button-----");
 		remote.onButtonWasPushed(0);
+		System.out.println("-----Push the partyOff button-----");
 		remote.offButtonWasPushed(0);
-		System.out.println(remote);
-		remote.undoButtonWasPushed();
-		//remote.onButtonWasPushed(1);
-		//remote.offButtonWasPushed(1);
-		//remote.onButtonWasPushed(2);
-		//remote.offButtonWasPushed(2);
-		//remote.undoButtonWasPushed();
-		//remote.onButtonWasPushed(3);
-		//remote.offButtonWasPushed(3);
-		remote.offButtonWasPushed(0);
-		remote.onButtonWasPushed(0);
-		System.out.println(remote);
-		remote.undoButtonWasPushed();
-		
-		
-		/*测试CeilingFan相关undo功能*/
-		CeilingFan livingRoomCeilingFan = new CeilingFan("Living Room");
-		
-		CeilingFanOnCommand ceilingFanOnCommand = new CeilingFanOnCommand(livingRoomCeilingFan);
-		CeilingFanHighCommand ceilingFanHighCommand = new CeilingFanHighCommand(livingRoomCeilingFan);
-		CeilingFanMediumCommand ceilingFanMediumCommand = new CeilingFanMediumCommand(livingRoomCeilingFan);
-		CeilingFanLowCommand ceilingFanLowCommand = new CeilingFanLowCommand(livingRoomCeilingFan);
-		CeilingFanOffCommand ceilingFanOffCommand = new CeilingFanOffCommand(livingRoomCeilingFan);
-		
-		remote.setCommand(0, ceilingFanOnCommand, ceilingFanOffCommand);
-		remote.setCommand(1, ceilingFanLowCommand, ceilingFanOffCommand);
-		remote.setCommand(2, ceilingFanMediumCommand, ceilingFanOffCommand);
-		remote.setCommand(3, ceilingFanHighCommand, ceilingFanOffCommand);
-		
-		remote.onButtonWasPushed(0);
-		remote.offButtonWasPushed(0);
-		System.out.println(remote);
-		remote.undoButtonWasPushed();//执行undo之后应该返回为low speed
-		
-		remote.onButtonWasPushed(3);
-		remote.onButtonWasPushed(2);
-		System.out.println(remote);
-		remote.undoButtonWasPushed();//执行之后应该返回到remote.onButtonWasPushed(3)这句，即high speed
 	}
 
 }
