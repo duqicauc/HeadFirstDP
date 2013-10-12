810402377519e52d4f24c9e71dab4e19704a4e67
杜琪 <duqi_2009@163.com>
2013-10-12 10:34:02 +0800

    添加CeilingFan的相应Command类
    用来封装CeilingFan对象以处理不同的action；
    在这里还用一个状态变量prevspeed记录undo操作之前的风扇转速

---
+++ src/command/remote/CeilingFanHighCommand.java
@@ -0,0 +1,40 @@
+package command.remote;
+
+public class CeilingFanHighCommand implements Command {
+	private CeilingFan ceilingFan;
+	private int prevSpeed; //记录undo之前的风扇转速
+	
+	public CeilingFanHighCommand(CeilingFan ceilingFan) {
+		this.ceilingFan = ceilingFan;
+	}
+	
+	@Override
+	public void execute() {
+		prevSpeed = ceilingFan.getSpeed();
+		ceilingFan.high();
+
+	}
+
+	@Override
+	public void undo() {
+		switch (prevSpeed) {
+		case CeilingFan.HIGH:
+			ceilingFan.high();
+			break;
+		case CeilingFan.MEDIUM:
+			ceilingFan.medium();
+			break;
+		case CeilingFan.LOW:
+			ceilingFan.low();
+			break;
+		case CeilingFan.OFF:
+			ceilingFan.off();
+			break;
+
+		default:
+			break;
+		}
+
+	}
+
+}
