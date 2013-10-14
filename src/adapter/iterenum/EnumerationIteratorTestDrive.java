ce0a36f3c2121ea9346162fd11dccd064e216b1e
杜琪 <duqi_2009@163.com>
2013-10-14 13:32:03 +0800

    完善EnumerationIteratorTestDrive类
    （1）给泛型定义添加类型String
    （2）添加注释解释Iterator接口的三个主要函数

--- src/adapter/iterenum/EnumerationIteratorTestDrive.java
+++ src/adapter/iterenum/EnumerationIteratorTestDrive.java
@@ -4,15 +4,20 @@ import java.util.*;
 
 public class EnumerationIteratorTestDrive {
 	public static void main(String[] args) {
-		Vector v = new Vector(Arrays.asList(args));
+		Vector<String> v = new Vector<String>(Arrays.asList(args));
 		v.add("duqi");
 		v.add("bdd");
 		v.add("liuman");
 		Iterator iterator = new EnumerationIterator(v.elements());
+		
+		/* 
+		 * hasNext()：判断后面还有没有元素
+		 * next():取得下一个元素
+		 * remove():删除当前元素，当然，在这里，对于EnumerationIterator来说会报异常
+		 * */
 		while (iterator.hasNext()) {
 			System.out.println(iterator.next());
 		}
-		iterator.remove();
-		//iterator.next();
+		//iterator.remove();
 	}
 }
