5e3a272bc41fb8aee52662de70977f3057272135
杜琪 <duqi_2009@163.com>
2013-10-17 09:34:09 +0800

    给Tea也添加使用Hook函数的代码

---
+++ src/template/simplebarista/TeaWithHook.java
@@ -0,0 +1,60 @@
+package template.simplebarista;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TeaWithHook extends CaffeineBeverageWithHook {
+ 
+//	public void prepareRecipe() {
+//		boilWater();
+//		steepTeaBag();
+//		pourInCup();
+//		addLemon();
+//	}
+ 
+//	public void boilWater() {
+//		System.out.println("Boiling water");
+//	}
+ 
+	public void brew() {
+		System.out.println("Steeping the tea");
+	}
+ 
+	public void addCondiments() {
+		System.out.println("Adding Lemon");
+	}
+ 
+//	public void pourInCup() {
+//		System.out.println("Pouring into cup");
+//	}
+	
+	public boolean customerWantsCondiments() {
+
+		String answer = getUserInput();
+
+		if (answer.toLowerCase().startsWith("y")) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+ 
+	private String getUserInput() {
+		// get the user's response
+		String answer = null;
+
+		System.out.print("Would you like Lemon with your tea (y/n)? ");
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			answer = in.readLine();
+		} catch (IOException ioe) {
+			System.err.println("IO error trying to read your answer");
+		}
+		if (answer == null) {
+			return "no";
+		}
+		return answer;
+	}
+}
