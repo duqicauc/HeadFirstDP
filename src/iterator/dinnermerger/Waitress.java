c3019321c726e2b6cb7aa0c23553bfb85cc3c46c
杜琪 <duqi_2009@163.com>
2013-10-18 11:50:27 +0800

    为Waittress添加了其他几个打印方法
    问题：
    针对具体类编程；重复代码多；耦合度高

--- src/iterator/dinnermerger/Waitress.java
+++ src/iterator/dinnermerger/Waitress.java
@@ -40,4 +40,55 @@ public class Waitress {
 			System.out.println(menuItem.getDescription());
 		}
 	}
+	/**
+	 * prints just breakfast items
+	 * */
+	public void printBreakfastMenu(){
+		PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
+		ArrayList breakfastItems = pancakeHouseMenu.getMenuItems();
+		for (int i = 0; i < breakfastItems.size(); i++) {
+			MenuItem menuItem = (MenuItem)breakfastItems.get(i);
+			System.out.print(menuItem.getName() + " ");
+			System.out.println(menuItem.getPrice() + " ");
+			System.out.println(menuItem.getDescription());
+		}
+	}
+	/**
+	 * prints just lunch items
+	 * */
+	public void printLunchMenu(){
+		DinnerMenu dinnerMenu = new DinnerMenu();
+		MenuItem[] lunchItems = dinnerMenu.getMenuItems();
+		for (int i = 0; i < lunchItems.length; i++) {
+			MenuItem menuItem = lunchItems[i];
+			System.out.print(menuItem.getName() + " ");
+			System.out.println(menuItem.getPrice() + " ");
+			System.out.println(menuItem.getDescription());
+		}
+	}
+	
+	/**
+	 * prints all vegetarian menu items
+	 * */
+	public void printVegetarianMenu(){
+		PancakeHouseMenu pancakeHouseMenu =  new PancakeHouseMenu();
+		ArrayList breakfastItems = pancakeHouseMenu.getMenuItems();
+		for (int i = 0; i < breakfastItems.size(); i++) {
+			MenuItem menuItem = (MenuItem)breakfastItems.get(i);
+			if (isItemVegetarian(menuItem)) {
+				System.out.print(menuItem.getName() + " ");
+				System.out.println(menuItem.getPrice() + " ");
+				System.out.println(menuItem.getDescription());
+			}
+		}
+	}
+	
+	/**
+	 * given the name of an item, returns true
+	 * if the items is vegetarian, otherwise,
+	 * return false
+	 * */
+	public boolean isItemVegetarian(MenuItem menuItem) {
+		return menuItem.isVegetarian();
+	}
 }
