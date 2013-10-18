619339df0e6c7bd8f96645aaa01465b6ce063001
杜琪 <duqi_2009@163.com>
2013-10-18 10:03:18 +0800

    添加“菜单项”类MenuItem

---
+++ src/iterator/dinnermerger/MenuItem.java
@@ -0,0 +1,83 @@
+package iterator.dinnermerger;
+
+/**
+ * 菜单项
+ * */
+public class MenuItem {
+	private String name;			//菜品名称
+	private String description;		//菜品描述
+	private boolean vegetarian;		//是否为素食者的标识
+	private double price;			//菜品价格
+	
+	/**
+	 * @param name
+	 * @param description
+	 * @param vegetarian
+	 * @param price
+	 */
+	public MenuItem(String name, String description, boolean vegetarian,
+			double price) {
+		super();
+		this.name = name;
+		this.description = description;
+		this.vegetarian = vegetarian;
+		this.price = price;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the vegetarian
+	 */
+	public boolean isVegetarian() {
+		return vegetarian;
+	}
+
+	/**
+	 * @param vegetarian the vegetarian to set
+	 */
+	public void setVegetarian(boolean vegetarian) {
+		this.vegetarian = vegetarian;
+	}
+
+	/**
+	 * @return the price
+	 */
+	public double getPrice() {
+		return price;
+	}
+
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+}
