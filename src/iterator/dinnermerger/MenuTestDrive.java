4253fb4f976485b5ef83566aa75377863236efe8
杜琪 <duqi_2009@163.com>
2013-10-18 15:09:50 +0800

    添加MenuTestDrive测试类

---
+++ src/iterator/dinnermerger/MenuTestDrive.java
@@ -0,0 +1,14 @@
+package iterator.dinnermerger;
+
+public class MenuTestDrive {
+	public static void main(String args[]) {
+        PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
+        DinnerMenu dinerMenu = new DinnerMenu();
+ 
+		Waitress waitress = new Waitress(dinerMenu, pancakeHouseMenu);
+ 
+		//waitress.printMenu();
+		//waitress.printBreakfastMenu();
+		waitress.printVegetarianMenu();
+	}
+}
