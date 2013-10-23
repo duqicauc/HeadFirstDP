25a1132e368cb673b0a8973fe5dba2326ba8f11b
杜琪 <duqi_2009@163.com>
2013-10-23 09:30:24 +0800

    修改DinnerMenuIterator使得它使用JavaAPI已经实现的迭代器
    在这里使用remove时要注意错误检查：（1）数组中已经没有元素，不能删除；（2）删除位置position的元素，将position+1的元素到最后一个集体前移

--- src/iterator/dinnermerger/DinnerMenuIterator.java
+++ src/iterator/dinnermerger/DinnerMenuIterator.java
@@ -1,5 +1,8 @@
 package iterator.dinnermerger;
 
+import java.util.Iterator;
+
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
 
 public class DinnerMenuIterator implements Iterator {
 	private MenuItem[] items;
@@ -29,6 +32,25 @@ public class DinnerMenuIterator implements Iterator {
 		MenuItem menuItem = items[position];
 		position += 1;
 		return menuItem;
+	}
+
+	/**
+	 * 删除，因为这里chef用了固定大小的数组来存放菜单项
+	 * 所以我们删除一个元素时，策略是将其后面的项一直到最后一项整体向前移位，将
+	 * 要删除的那个元素覆盖掉
+	 * */
+	@Override
+	public void remove() {
+		if (position <= 0) {
+			throw new IllegalStateException
+				("You can't remove an item until you've done at least one next()");
+		}
+		if (items[position-1] != null) {
+			for (int i = position - 1; i < (items.length-1); i++) {
+				items[i] = items[i+1];
+			}
+			items[items.length-1] = null;
+		}
 	}
 
 }
