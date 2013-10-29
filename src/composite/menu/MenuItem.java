eb8bbb8045a756a8cd6e547e135eeed4fa25926c
杜琪 <duqi_2009@163.com>
2013-10-29 10:47:37 +0800

    添加MenuItem类，继承自MenuComponent类
    MenuItem支持print方法，因此只需要重写父类的print方法，其他方法不支持，虽然也继承来，但是父类中默认实现是抛出“不支持异常”

---
+++ src/composite/menu/MenuItem.java
@@ -0,0 +1,59 @@
+package composite.menu;
+
+public class MenuItem extends MenuComponent {
+	private String	name;
+	private String	description;
+	private boolean vegetarian;
+	private double 	price;
+	/**
+	 * @param name
+	 * @param description
+	 * @param vegetarian
+	 * @param price
+	 */
+	public MenuItem(String name, String description, boolean vegetarian,
+			double price) {
+		super();
+		this.name = name;
+		this.description = description;
+		this.vegetarian = vegetarian;
+		this.price = price;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	/**
+	 * @return the vegetarian
+	 */
+	public boolean isVegetarian() {
+		return vegetarian;
+	}
+	/**
+	 * @return the price
+	 */
+	public double getPrice() {
+		return price;
+	}
+	
+	/**
+	 * 在MenuComponent中有默认实现
+	 * 在这里针对MenuItem实现属于它自己特点的print函数
+	 * */
+	public void print(){
+		System.out.print("  " + getName());
+		if (isVegetarian()) {
+			System.out.print("(v)");
+		}
+		System.out.println(", " + getPrice());
+		System.out.println("     -- " + getDescription());
+	}
+}
