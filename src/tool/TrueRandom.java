c81ae5eced357d73b8389eb3bfea31c948f43313
杜琪 <duqi_2009@163.com>
2013-10-30 17:29:11 +0800

    对TrueRandom做进一步修改
    在《编程珠玑》中有个问题：生成[n,m]之间不可重复的k个数，可以用这个方法来搞哦

--- src/tool/TrueRandom.java
+++ src/tool/TrueRandom.java
@@ -3,8 +3,11 @@ package tool;
 import java.util.Random;
 
 public class TrueRandom {
+	/**
+	 * 生成[n,m]之间的随机数
+	 * 注意，不是“伪随机数”
+	 * */
 	public static int randomNToM(int n, int m) {
-		int randomNumber;
 		int lowerbound = 0;
 		int upperbound = 9;
 		int lastindex = upperbound - lowerbound + 1;
@@ -12,12 +15,6 @@ public class TrueRandom {
 		Random random = new Random();
 		for (int i = lowerbound; i <= upperbound; i++)
 			rndavailable[i - lowerbound] = i;
-//		for (int i = 1; i <= 10; i++) {
-//			int rndindex = random1.nextInt(lastindex);
-//			System.out.println("第" + i + "个随机数:" + rndavailable[rndindex]);
-//			rndavailable[rndindex] = rndavailable[lastindex - 1];
-//			lastindex--;
-//		}
 		int rndindex = random.nextInt(lastindex);
 		return rndavailable[rndindex];
 	}
