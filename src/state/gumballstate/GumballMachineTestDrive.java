1661a7f772a9b9065989ec22a8fdda18ff88346e
杜琪 <duqi_2009@163.com>
2013-10-31 16:04:35 +0800

    修改GumballMachineTestDrive，利用rmi绑定服务
    关键语句：
    Naming.rebind(**,*)

--- src/state/gumballstate/GumballMachineTestDrive.java
+++ src/state/gumballstate/GumballMachineTestDrive.java
@@ -1,16 +1,33 @@
 package state.gumballstate;
 
+import java.rmi.*;
+
+
 public class GumballMachineTestDrive {
 
 	public static void main(String[] args) {
-		GumballMachine gumballMachine = new GumballMachine("白堤路",5); //预装5颗糖果
+		GumballMachineRemote gumballMachine = null;
+		int count;
+		if (args.length < 2) {
+			System.out.println("GumballMachine <name> <inventory>");
+			System.exit(1);
+		}
+		
+		try {
+			count = Integer.parseInt(args[1]);
+			// remote service 实例化一个GumballMachine，并需要用RMI注册
+			gumballMachine = new GumballMachine(args[0],count);
+			Naming.rebind("//" + args[0] + "/gumballmachine",gumballMachine);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
 
-		System.out.println(gumballMachine);
-
-		gumballMachine.insertQuarter();
-		gumballMachine.turnCrank();
-
-		System.out.println(gumballMachine);
+//		System.out.println(gumballMachine);
+//
+//		gumballMachine.insertQuarter();
+//		gumballMachine.turnCrank();
+//
+//		System.out.println(gumballMachine);
 
 //		gumballMachine.insertQuarter();
 //		gumballMachine.turnCrank();
